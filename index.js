a5da15dc1859d6047553f3ca90dbeb8ba9983821
Amer Mohammed <ameralkhanni@gmail.com>
2023-01-29 20:42:05 +0300

    print info.. car

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ var car3 = {
         console.log(" Honk very high");
     }
 };
-// console.log(car1);
-// console.log(car2);
-// console.log(car3);
-// car1.honk();
-// car2.honk();
-// car3.honk();
-// console.log(car1.year);
-// console.log(car2.year);
-// console.log(car3.year);
-// console.log(car1.model);
-// console.log(car2.model);
-// console.log(car3.model);
+console.log(car1);
+console.log(car2);
+console.log(car3);
+car1.honk();
+car2.honk();
+car3.honk();
+console.log(car1.year);
+console.log(car2.year);
+console.log(car3.year);
+console.log(car1.model);
+console.log(car2.model);
+console.log(car3.model);
--- index.ts
+++ index.ts
@@ -36,21 +36,21 @@ let car3:car={
    
 }
 
-// console.log(car1);
-// console.log(car2);
-// console.log(car3);
+console.log(car1);
+console.log(car2);
+console.log(car3);
 
-// car1.honk();
-// car2.honk();
-// car3.honk();
+car1.honk();
+car2.honk();
+car3.honk();
 
-// console.log(car1.year);
-// console.log(car2.year);
-// console.log(car3.year);
+console.log(car1.year);
+console.log(car2.year);
+console.log(car3.year);
 
-// console.log(car1.model);
-// console.log(car2.model);
-// console.log(car3.model);
+console.log(car1.model);
+console.log(car2.model);
+console.log(car3.model);
 
 
 
